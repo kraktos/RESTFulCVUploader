7e31d6d9a9ac95f026b4b2e539b3614e11d745c8
kraktos <arnab.dutta@gmail.com>
2015-03-24 10:56:48 +0100

    adding mods with response

--- src/main/java/ApacheHttpRestClient.java
+++ src/main/java/ApacheHttpRestClient.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -15,12 +18,11 @@ public class ApacheHttpRestClient {
 		File file = new File("/home/adutta/cv_Dutta.pdf");
 		// Upload the file
 		fileUpload.executeMultiPartRequest(
-				"http://cvu.zanox.com/rest/upload/cv", file, file.getName(),
-				"File Uploaded :: cv_Dutta.pdf");
+				"http://cvu.zanox.com/rest/upload/cv", file);
 	}
 
-	public void executeMultiPartRequest(String urlString, File file,
-			String fileName, String fileDescription) throws Exception {
+	public void executeMultiPartRequest(String urlString, File file)
+			throws Exception {
 		HttpClient client = HttpClientBuilder.create().build();
 
 		HttpPost postRequest = new HttpPost(urlString);
@@ -53,11 +55,23 @@ public class ApacheHttpRestClient {
 			// Verify response if any
 			if (response != null) {
 				System.out.println(response.getStatusLine().getStatusCode());
-				System.out.println(response.toString());
+				System.out.println(getContent(response));
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+
+	public static String getContent(HttpResponse response) throws IOException {
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response
+				.getEntity().getContent()));
+		String body = "";
+		String content = "";
+
+		while ((body = rd.readLine()) != null) {
+			content += body + "\n";
+		}
+		return content.trim();
 	}
 
 }
